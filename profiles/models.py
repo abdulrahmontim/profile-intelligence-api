2acdb19196c0c6faaaa2888319784986f10bd22f
Abdulrahmon <abdulrahmonsulaimon60@gmail.com>
2026-04-15 18:33:07 +0100

    implemented serializer

--- profiles/models.py
+++ profiles/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
        ("female", "Female")
     ]
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
-    gender_probability = models.DecimalField()
+    gender_probability = models.DecimalField(max_digits=5, decimal_places=4)
     sample_size = models.IntegerField()
     age = models.IntegerField()
     
@@ -23,7 +23,7 @@ class Profile(models.Model):
     ]
     age_group = models.CharField(max_length=10, choices=AGE_GROUPS)
     country_id = models.CharField(max_length=10)
-    country_probability = models.DecimalField(max_digit=5, decimal_places=4)
+    country_probability = models.DecimalField(max_digits=5, decimal_places=4)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
