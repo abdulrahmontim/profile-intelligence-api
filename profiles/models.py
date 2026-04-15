0ecc7d387c703957042abe1ec8b41043328a80d0
Abdulrahmon <abdulrahmonsulaimon60@gmail.com>
2026-04-15 03:46:36 +0100

    Implemented profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,31 @@
 from django.db import models
+from uuid6 import uuid7
 
 # Create your models here.
-
+class Profile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid7, editable=False)
+    name = models.CharField(max_length=64, unique=True)
+    
+    GENDER_CHOICES = [
+       ("male", "Male"),
+       ("female", "Female")
+    ]
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    gender_probability = models.DecimalField()
+    sample_size = models.IntegerField()
+    age = models.IntegerField()
+    
+    AGE_GROUPS = [
+        ("child", "Child"),
+        ("teenager", "Teenager"),
+       ( "adult", "Adult"),
+       ( "senior", "Senior")
+    ]
+    age_group = models.CharField(max_length=10, choices=AGE_GROUPS)
+    country_id = models.CharField(max_length=10)
+    country_probability = models.DecimalField(max_digit=5, decimal_places=4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
