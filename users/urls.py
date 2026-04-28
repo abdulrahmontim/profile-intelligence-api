4400fb05c03de538a9961e5a8d60f78c8aa4880e
Abdulrahmon <abdulrahmonsulaimon60@gmail.com>
2026-04-28 23:40:22 +0100

    add user model to settings

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("github", views.GithubLoginView.as_view(), name="github_login"),
-    path("github/callback", views.GithubCallbackView, name="github_callback"),
+    path("github/callback", views.GithubCallbackView.as_view(), name="github_callback"),
     path("refresh", views.GithubRefreshView.as_view(), name="refresh"),
     path("logout", views.GithubLogoutView.as_view(), name="logout")
 ]
