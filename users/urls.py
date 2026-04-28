4b2764d46eef8e742b1957ef67fe0d91c365f5f3
Abdulrahmon <abdulrahmonsulaimon60@gmail.com>
2026-04-28 23:36:28 +0100

    add route name to github route

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("github", views.GithubLoginView.as_view(), "github_login"),
+    path("github", views.GithubLoginView.as_view(), name="github_login"),
     path("github/callback", views.GithubCallbackView, name="github_callback"),
     path("refresh", views.GithubRefreshView.as_view(), name="refresh"),
     path("logout", views.GithubLogoutView.as_view(), name="logout")
