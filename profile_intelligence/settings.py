da7bfa99c32abb80246775de1c37b390bb4c1808
Abdulrahmon <abdulrahmonsulaimon60@gmail.com>
2026-04-29 06:53:29 +0100

    feat(middleware): add jwt middleware to settings

--- profile_intelligence/settings.py
+++ profile_intelligence/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'users.middleware.JWTAuthMiddleware'
 ]
 
 ROOT_URLCONF = 'profile_intelligence.urls'
